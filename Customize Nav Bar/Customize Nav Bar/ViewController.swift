3a65a580a342d21548cd4a3198e1dbf1a09498b2
Fauzee26 <32213481+Fauzee26@users.noreply.github.com>
2017-10-10 11:37:41 +0700

    Add navigation bar and change color bar button and add new image

--- Customize Nav Bar/Customize Nav Bar/ViewController.swift
+++ Customize Nav Bar/Customize Nav Bar/ViewController.swift
@@ -19,7 +19,29 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    //add function did appear
+    override func viewDidAppear(_ animated: Bool) {
+        
+        //deklarasi nav sebagai navigation bar
+        let nav = self.navigationController?.navigationBar
+        
+        //tambahkan style bar
+        nav?.barStyle = UIBarStyle.black
+        nav?.tintColor = UIColor.yellow
+        
+        //menambahkan gambar atau logo
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
+        imageView.contentMode = .scaleAspectFill
+        
+        //memanggil nama gambarNya
+        let image = UIImage(named: "android")
+        //menampilkan image ke imageView
+        imageView.image = image
+        
+        //menampilkan title navigation item sebagai imageView
+        navigationItem.titleView = imageView
+    
 
 }
 
+}
